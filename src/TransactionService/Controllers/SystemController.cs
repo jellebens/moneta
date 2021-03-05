a82ee0d1cbb9457562d2d03e838f8c868d1b3444
Jelle Bens <jelle.bens@portima.onmicrosoft.com>
2021-03-05 22:50:08 +0100

    Added Transactions Service

---
+++ src/TransactionService/Controllers/SystemController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        private readonly ILogger<SystemController> _Logger;
+
+        public SystemController(ILogger<SystemController> logger)
+        {
+            this._Logger = logger;
+        }
+
+        [HttpGet]
+        [Route("live")]
+        public ActionResult IsLive()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("ready")]
+        public ActionResult IsReady()
+        {
+            return Ok();
+        }
+    }
+}
