4452c0e2db07b1b00f7008ad7979faccbaa8ce92
Jelle Bens <jellebens@outlook.com>
2021-04-13 17:09:09 +0200

    List of accounts

--- src/Moneta.Frontend.Web/Controllers/AccountsController.cs
+++ src/Moneta.Frontend.Web/Controllers/AccountsController.cs
@@ -16,17 +16,31 @@ namespace Moneta.Frontend.Web.Controllers
         private readonly ILogger<AccountsController> _Logger;
         private readonly IAccountsService _AccountService;
         private readonly IJwtTokenBuilder _JwtTokenBuilder;
+        private readonly IAccountsService _AccountsService;
 
-        public AccountsController(ILogger<AccountsController> logger, IAccountsService accountService, IJwtTokenBuilder jwtTokenBuilder)
+        public AccountsController(ILogger<AccountsController> logger, IAccountsService accountService, IJwtTokenBuilder jwtTokenBuilder, IAccountsService accountsService)
         {
             _Logger = logger;
             _AccountService = accountService;
             _JwtTokenBuilder = jwtTokenBuilder;
+            _AccountsService = accountsService;
         }
+        
         [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public async Task<IActionResult> List()
+        {
+            _AccountsService.Authenticate(_JwtTokenBuilder.Build(User));
+
+            AccountInfo[] accounts = await _AccountsService.ListAsync();
+
+            return new JsonResult(accounts);
         }
 
         [HttpPost]
