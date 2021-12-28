dcf7baa9ee0b746579004b58ae245707c0ab7384
Jelle Bens <jellebens@outlook.com>
2021-12-28 21:05:02 +0100

    Added Account controller

---
+++ src/Moneta.Frontend.API/Controllers/AccountsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Moneta.Frontend.API.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger<AccountsController> _Logger;
+
+        public AccountsController(ILogger<AccountsController> logger)
+        {
+            _Logger = logger;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Get()
+        {
+            var accounts = new[]{
+                                    new { name = "mock account 1", currency = "EUR"},
+                                    new { name = "mock account 2", currency = "USD"},
+                                };
+
+            return Ok(accounts);
+        }
+    }
+}
