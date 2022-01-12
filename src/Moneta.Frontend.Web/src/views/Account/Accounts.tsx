0c0ed78ac19cf0d65b68c020c93ec478536dacc2
Jelle Bens <jellebens@outlook.com>
2022-01-12 20:44:09 +0100

    Create account UI logic done

--- src/Moneta.Frontend.Web/src/views/Account/Accounts.tsx
+++ src/Moneta.Frontend.Web/src/views/Account/Accounts.tsx
@@ -1,31 +1,27 @@
 import axios from "axios";
 
-export interface AccountListItem{
+export interface AccountListItem {
     name: string
     currency: string
 }
 
 export const dummyAccounts: AccountListItem[] = [
-    {name: "mock account 1", currency: "EUR"},
-    {name: "mock account 2", currency: "USD"}
+    { name: "mock account 1", currency: "EUR" },
+    { name: "mock account 2", currency: "USD" }
 ]
 
-export const AccountsList = async (token : string) : Promise<AccountListItem[]> => {
+export const AccountsList = async (token: string): Promise<AccountListItem[]> => {
     let url = "/api/accounts";
-           
-                const config = {
-                    headers: { Authorization: `Bearer ${token}` },
-                    mode: "no-cors",
-                };
-                
-                return await (await axios.get(url, config)).data;
-    
 
-    
-    return dummyAccounts;
+    const config = {
+        headers: { Authorization: `Bearer ${token}` },
+        mode: "no-cors",
+    };
+
+    return await (await axios.get(url, config)).data;
 }
 
-const wait = (ms: number):Promise<void> => {
+const wait = (ms: number): Promise<void> => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
--- src/Moneta.Frontend.Web/src/views/Account/New.tsx
+++ src/Moneta.Frontend.Web/src/views/Account/New.tsx
@@ -11,84 +11,114 @@ import {
     Input,
 
 } from "reactstrap";
-
-
+import axios from "axios";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { AccountInfo, NavigationClient } from "@azure/msal-browser";
+import { loginRequest } from "AuthConfig";
+import { useHistory } from "react-router-dom";
 
 export const NewAccountView = () => {
-
+    const { instance, accounts } = useMsal();
     const [accountName, setAccountName] = useState("");
     const [currency, setCurrency] = useState("EUR");
+    const [IsSubmitted, setIsSubmitted] = React.useState(false);
+    const history = useHistory();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
 
 
-        if(accountName !== ""){
+        if (accountName !== "") {
             await submitForm();
         }
     };
 
+
+
+    const submitForm = async () => {
         
 
-     const submitForm = async() : Promise<boolean> => {
-        // TODO - submit the form
-        
-        return true;
-}
+        const request = {
+            scopes: loginRequest.scopes,
+            account: accounts[0] as AccountInfo
+        };
+
+        instance.acquireTokenSilent(request).then(async (response) => {
+            
+            setIsSubmitted(true);
+
+            let url = "/api/accounts";
+
+            const config = {
+                headers: { Authorization: `Bearer ${response.accessToken}` },
+                mode: "no-cors",
+            };
+
+            await (await axios.post(url,{
+                "Name": accountName,
+                "Currency": currency
+            } ,config));
+            history.push("/accounts")
+        }).catch((e) => {
+            console.log(e);
+        });
+
+    }
 
 
-return (
-    <Row>
-        <Col md="12">
-            <Card>
-                <CardHeader>
-                    <CardTitle tag="h4">Create new Account</CardTitle>
-                </CardHeader>
-                <CardBody>
-                    <Form onSubmit={handleSubmit} noValidate={true}>
-                        <Row>
-                            <Col md="12">
-                                <FormGroup>
-                                    <label>Name</label>
-                                    <Input type="text" value={accountName} onChange={(e) => setAccountName(e.target.value)} />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md="12">
-                                <FormGroup>
-                                    <label>Currency</label>
-                                    <Input type={"select"} value={currency} onChange={(e) => setCurrency(e.target.value)}>
-                                        <option>EUR</option>
-                                        <option>USD</option>
-                                    </Input>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <div className="update ml-auto mr-auto">
-                                <Button
-                                    className="btn-round"
-                                    color="danger"
-                                >
-                                    Cancel
-                                </Button>
-                                <Button
-                                    className="btn-round"
-                                    color="primary"
-                                    type="submit"
-                                >
-                                    Save
-                                </Button>
-                            </div>
-                        </Row>
-                    </Form>
-                </CardBody>
-            </Card>
-        </Col>
-    </Row>
+    return (
+        <Row>
+            <Col md="12">
+                <Card>
+                    <CardHeader>
+                        <CardTitle tag="h4">Create new Account</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <Form onSubmit={handleSubmit} noValidate={true}>
+                            <Row>
+                                <Col md="12">
+                                    <FormGroup>
+                                        <label>Name</label>
+                                        <Input type="text" value={accountName} onChange={(e) => setAccountName(e.target.value)} />
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md="12">
+                                    <FormGroup>
+                                        <label>Currency</label>
+                                        <Input type={"select"} value={currency} onChange={(e) => setCurrency(e.target.value)}>
+                                            <option>EUR</option>
+                                            <option>USD</option>
+                                        </Input>
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <div className="update ml-auto mr-auto">
+                                    <Button
+                                        className="btn-round"
+                                        color="danger"
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button
+                                        className="btn-round"
+                                        color="primary"
+                                        type="submit"
+                                        disabled={IsSubmitted}
+                                    >
+                                        Save
+                                    </Button>
+                                </div>
+                            </Row>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
 
-);
+    );
 }
 
 
